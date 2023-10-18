1d03ce48c95c8d174f1d60f52bf89dc4a1099a0a
marciomartins12 <marciomartins12@pop-os.localdomain>
2023-10-18 15:35:00 -0300

    responsivo

--- src/Pages/PaginaLogin/index.jsx
+++ src/Pages/PaginaLogin/index.jsx
@@ -3,8 +3,8 @@ import Formulario from '../../Components/Formulario';
 import Funcao from '../../Components/FuncoesEntrrarEsqueceuAsenhaEcadastrar';
 import Links from "../../Components/Links";
 import Footer from "../../Components/Footer";
-import style from "./PaginaLogin.module.css";
 import img from "../../assets/img/imgCelular.png";
+import style from "./PaginaLogin.module.css";
 
 const PaginaLogin = () => {
     //   alert("Atenção! isso é um phishing, não coloque sua senha ou email verdadeiro. os dados salvos serão apagados em 1hr mas se você mesmo quiser apagar, o email e senha do banco de dados estará disponivel no readme do github marciomartins12 repositorio instagram")
@@ -27,7 +27,7 @@ const PaginaLogin = () => {
                         <Funcao />
                     </div>
                     <div className={style.semConta}>
-                        <h6 className={style.semConta}>não tem uma conta? <a href="#">Cadastre-se</a></h6>
+                        <h6>não tem uma conta? <a href="#">Cadastre-se</a></h6>
                     </div>
 
                     <div className={style.divAplicativos}>
